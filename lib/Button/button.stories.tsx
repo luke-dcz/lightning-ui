dffd4432b392e6a559970bd78b0fc6c054e383a8
luke-dcz <luke.dacruz.2020@live.rhul.ac.uk>
2024-06-07 00:49:17 +0200

    add story for custom style

--- lib/Button/button.stories.tsx
+++ lib/Button/button.stories.tsx
@@ -11,6 +11,13 @@ type Story = StoryObj<typeof Button>
 
 export const Default: Story = {
 	args: {
-		children: 'Click Me'
+		children: "Click Me",
+	}
+}
+
+export const CustomStyles: Story = {
+	args: {
+		children: "Click Me",
+		className: "bg-pink-500 hover:bg-pink-400"
 	}
 }
