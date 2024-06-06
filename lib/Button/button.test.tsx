9a8d9ac1e7a0d78c9d72902a8a74ea7f87439ff6
luke-dcz <luke.dacruz.2020@live.rhul.ac.uk>
2024-06-07 00:30:15 +0200

    add new tests for button component

--- lib/Button/button.test.tsx
+++ lib/Button/button.test.tsx
@@ -8,7 +8,7 @@ describe(`Component: ${Button.name}`, () => {
 		expect(container).toMatchInlineSnapshot(`
 			<div>
 			  <button
-			    class="bg-blue-500"
+			    class="px-4 py-2 rounded-lg bg-default"
 			  >
 			    My Button
 			  </button>
@@ -20,12 +20,26 @@ describe(`Component: ${Button.name}`, () => {
 
 	beforeEach(() => {
 		render(
-			<Button>Test</Button>
+			<Button data-testid='button'>Test</Button>
 		)
 		buttonText = screen.getByText('Test')
 	})
 
 	it('renders the child element', () => {
 		expect(buttonText).toBeInTheDocument()
+	})
+
+	it('has the default color background', () => {
+		expect(screen.getByTestId('button')).toHaveClass("bg-default")
+	})
+
+	it('has custom styles given through the className prop', () => {
+		render(
+			<Button data-testid="button2" className="bg-orange-500 rounded-full">
+				Test
+			</Button>
+		)
+		expect(screen.getByTestId('button2')).toHaveClass('bg-orange-500 rounded-full')
+		expect(screen.getByTestId('button2')).not.toHaveClass('bg-default rounded-lg')
 	})
 })
