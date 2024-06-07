82a0c6d984a3ca6e8a51ffa22fa03196b07804ba
luke-dcz <luke.dacruz.2020@live.rhul.ac.uk>
2024-06-07 18:48:56 +0200

    add isDisabled prop

--- lib/Button/button.tsx
+++ lib/Button/button.tsx
@@ -4,15 +4,18 @@ import { twMerge } from "tailwind-merge";
 type ButtonProps = {
 	className?: string
 	children: ReactNode;
+	isDisabled?: boolean;
 } & ComponentProps<'button'>
 
 //TO-DO - Variants prop, size prop, color scheme prop (using predefined color scheme)
 
-export const Button = ({ className = '', children, ...restprops }: ButtonProps) => {
+export const Button = ({ className = '', children, isDisabled = false, ...restprops }: ButtonProps) => {
+	const disabledStyle = isDisabled && 'cursor-not-allowed'
 	return (
 		<button
 			className={twMerge(
-				'px-4 py-2 rounded-lg bg-default text-white hover:bg-default-dark transition-all ease-in-out duration-3	00',
+				'px-4 py-2 rounded-lg bg-default text-white hover:bg-default-dark transition-all ease-in-out duration-300',
+				disabledStyle,
 				className
 			)}
 			{...restprops}
