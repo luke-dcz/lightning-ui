6df01841c4055a1bb2c15996d2b959c19fd5d33c
luke-dcz <luke.dacruz.2020@live.rhul.ac.uk>
2024-06-09 20:18:01 +0200

    add size prop to chip component

--- lib/Chip/chip.tsx
+++ lib/Chip/chip.tsx
@@ -4,17 +4,23 @@ import { twMerge } from "tailwind-merge";
 type ChipProps = {
 	children: ReactNode;
 	className?: string;
+	size?: 'sm' | 'md' | 'lg'
 }
 
-export const Chip = ({ children, className = '' }: ChipProps) => {
+export const Chip = ({ children, className = '', size = 'md' }: ChipProps) => {
+	const sizeStyle = size === 'sm' ? 'text-sm h-6 px-1'
+		: size === 'md' ? 'text-base h-7 px-1'
+			: 'text-lg h-8 px-2'
+
 	return (
 		<div
 			className={twMerge(
-				'flex bg-default text-white rounded-full items-center justify-center min-w-min max-w-fit relative box-border px-1 h-7 whitespace-nowrap',
+				'flex bg-default text-white rounded-full items-center justify-center min-w-min max-w-fit relative box-border whitespace-nowrap',
+				sizeStyle,
 				className
 			)}
 		>
-			<span className="flex text-inherit font-normal px-2">
+			<span className="flex text-inherit font-light px-2">
 				{children}
 			</span>
 		</div>
