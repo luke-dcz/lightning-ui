d101277f51a0a930a85a777c0b14bb7178387095
luke-dcz <luke.dacruz.2020@live.rhul.ac.uk>
2024-06-09 20:45:51 +0200

    add radius prop

--- lib/Chip/chip.tsx
+++ lib/Chip/chip.tsx
@@ -4,10 +4,20 @@ import { twMerge } from "tailwind-merge";
 type ChipProps = {
 	children: ReactNode;
 	className?: string;
+	radius?: "none" | "sm" | "base" | "md" | "lg" | "xl" | "full";
 	size?: 'sm' | 'md' | 'lg'
 }
 
-export const Chip = ({ children, className = '', size = 'md' }: ChipProps) => {
+export const Chip = ({ children, className = '', radius = "full", size = 'md' }: ChipProps) => {
+	const radiusStyles =
+		radius === 'none' ? 'rounded-none'
+			: radius === 'sm' ? 'rounded-sm'
+				: radius === 'base' ? 'rounded'
+					: radius === 'md' ? 'rounded-md'
+						: radius === 'lg' ? 'rounded-lg'
+							: radius === 'xl' ? 'rounded-xl'
+								: 'rounded-full'
+
 	const sizeStyle = size === 'sm' ? 'text-sm h-6 px-1'
 		: size === 'md' ? 'text-base h-7 px-1'
 			: 'text-lg h-8 px-2'
@@ -15,8 +25,9 @@ export const Chip = ({ children, className = '', size = 'md' }: ChipProps) => {
 	return (
 		<div
 			className={twMerge(
-				'flex bg-default text-white rounded-full items-center justify-center min-w-min max-w-fit relative box-border whitespace-nowrap',
+				'flex bg-default text-white items-center justify-center min-w-min max-w-fit relative box-border whitespace-nowrap',
 				sizeStyle,
+				radiusStyles,
 				className
 			)}
 		>
