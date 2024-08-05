c1b0ee92a6b793e352076b4461befdcfd21ff95a
luke-dcz <luke.dacruz.2020@live.rhul.ac.uk>
2024-08-05 18:58:35 +0200

    add multiple options prop

--- lib/Select/select.tsx
+++ lib/Select/select.tsx
@@ -8,6 +8,7 @@ type SelectProps = {
 		label: string
 	}[]
 	label?: string
+	multipleOptions?: boolean
 }
 
 const itemVariants: Variants = {
@@ -19,36 +20,56 @@ const itemVariants: Variants = {
 	closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
 };
 
-export const Select = ({ options, label = "Select Value" }: SelectProps) => {
+export const Select = ({ label = "Select Value", multipleOptions = false, options }: SelectProps) => {
 	const [showOptions, setShowOptions] = useState(false)
-	const [selectedOption, setSelectedOption] = useState<string>('')
+	const [selectedOptions, setSelectedOptions] = useState<string[]>([])
 
 	const toggleShowOptions = () => {
 		setShowOptions(!showOptions)
 	}
 
-	const handleSelectOption = (value: string) => {
-		setSelectedOption(value)
-		setShowOptions(false)
+
+	const handleSelectOptions = (value: string) => {
+		if (multipleOptions) {
+			setSelectedOptions(
+				[...selectedOptions, value]
+			)
+		} else {
+			setSelectedOptions([value])
+			setShowOptions(false)
+		}
+	}
+
+	const removeItem = (index: number, e: React.MouseEvent) => {
+		if (e && e.stopPropagation) e.stopPropagation()
+		setSelectedOptions(
+			selectedOptions.filter(item =>
+				selectedOptions.indexOf(item) !== index
+			)
+		)
 	}
 
 	const optionsStyle = showOptions ? 'flex' : 'hidden'
 
 	return (
 		<motion.div initial={false} animate={showOptions ? "open" : "closed"} className="flex flex-col min-w-fit max-w-fit gap-1">
-			<div className="flex items-center h-6 border-2 border-default-light dark:border-default hover:border-default-light focus:outline-black dark:focus:outline-white rounded px-2 py-5 hover:cursor-pointer" onClick={() => toggleShowOptions()}>
+			<button className="flex items-center h-6 border-2 border-default-light dark:border-default hover:border-default-light focus:outline-black dark:focus:outline-white rounded px-2 py-5 hover:cursor-pointer" onClick={() => toggleShowOptions()}>
 				<div className="flex justify-between items-center gap-2">
-					{selectedOption === '' ? (
+					{selectedOptions.length === 0 ? (
 						<p className="text-slate-700">{label}</p>
 					) : (
-						<div className="flex px-1 border rounded gap-1 justify-between items-center">
-							<p>{selectedOption}</p>
-							<div className='rounded hover:bg-zinc-200 transition-all ease-in-out duration-150' onClick={() => setSelectedOption('')}>
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
-									<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-								</svg>
-							</div>
-						</div>
+						<>
+							{selectedOptions.map((item, index) => (
+								<div className="flex px-1 border rounded gap-1 justify-between items-center">
+									<p>{item}</p>
+									<div className='rounded hover:bg-zinc-200 transition-all ease-in-out duration-150' onClick={(e) => removeItem(index, e)}>
+										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+											<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+										</svg>
+									</div>
+								</div>
+							))}
+						</>
 					)}
 					<motion.div
 						variants={{
@@ -63,7 +84,7 @@ export const Select = ({ options, label = "Select Value" }: SelectProps) => {
 						</svg>
 					</motion.div>
 				</div>
-			</div>
+			</button>
 			<motion.ul className={twMerge(
 				'flex flex-col px-1 py-1 border-2 rounded border-default-light dark:border-default',
 				optionsStyle
@@ -92,7 +113,7 @@ export const Select = ({ options, label = "Select Value" }: SelectProps) => {
 				style={{ pointerEvents: showOptions ? "auto" : "none" }}
 			>
 				{options.map((item) => (
-					<motion.li className='px-2 rounded cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-600 transition-all ease-in-out duration-150' variants={itemVariants} key={item.value} onClick={() => handleSelectOption(item.label)}>{item.label}</motion.li>
+					<motion.li className='px-2 rounded cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-600 transition-all ease-in-out duration-150' variants={itemVariants} key={item.value} onClick={() => handleSelectOptions(item.label)}>{item.label}</motion.li>
 				))}
 			</motion.ul>
 		</motion.div >
